7bd42ad30dfab79903a13ea6ab5ab69a408845a3
Kayan <correakayan06@gmail.com>
2024-05-03 13:25:41 -0300

    segundo commit

---
+++ app/src/main/java/com/example/gestao_de_contato_tela1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.gestao_de_contato_tela1
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+      val tela1 = findViewById<ImageView>(R.id.Main_Activity_tela1)
+    }
+
+}
